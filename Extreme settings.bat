19e2dfc31d3a1d89a4a86248e003f586369fb5ca
Aatrick <113598245+Aatrick@users.noreply.github.com>
2024-03-04 12:36:37 +0100

    Update Extreme settings.bat

--- Extreme settings.bat
+++ Extreme settings.bat
@@ -3,8 +3,9 @@ adb shell wm density 280
 
 adb shell dumpsys deviceidle force-idle
 
-adb shell settings put global adaptive_battery_management_enabled 1
 adb shell settings put system multicore_packet_scheduler 1
+adb shell settings put global sem_power_mode_limited_apps_and_home_screen 1
+adb shell settings put global apm_enhancement_enabled 0
 
 adb shell settings put global ble_scan_always_enabled 0
 adb shell settings put system nearby_scanning_enabled 0
@@ -19,28 +20,18 @@ adb shell settings put system mcf_continuity 0
 adb shell settings put system mcf_continuity_permission_denied 1
 adb shell settings put system mcf_permission_denied 1
 
-adb shell settings put system intelligent_sleep_mode 1
-adb shell settings put secure adaptive_sleep 1
-
-adb shell settings put global bixby_pregranted_permissions 0
-
-adb shell settings put system rakuten_denwa 0
 adb shell settings put system remote_control 0
 adb shell settings put system send_security_reports 0
 
-adb shell settings put global sem_enhanced_cpu_responsiveness 0
 
-adb shell settings put global restricted_device_performance 1,1
+adb shell settings put global restricted_device_performance 10,10
 
-adb shell cmd power set-fixed-performance-mode-enabled 1
+adb shell cmd power set-fixed-performance-mode-enabled 0
 adb shell cmd power set-mode 1
 
 adb shell settings put global zram 0 default 
 adb shell settings put global zram_enabled 0
 
-adb shell settings put global default windows_antimation_scale 0.25 
-adb shell settings put global default transition_animation_scale 0.25 
-adb shell settings put global default animator_duration_scale 0.25 
 adb shell settings put global ram_expand_size 0
 
 adb shell settings put global protect_battery 1
@@ -56,23 +47,23 @@ adb shell settings put global app_standby_enabled 0
 
 adb shell device_config put activity_manager bg_prompt_fgs_on_long_running 1
 adb shell device_config put activity_manager bg_current_drain_auto_restrict_abusive_apps_enabled 1
-adb shell device_config put app_hibernation app_hibernation_enabled 1
+adb shell device_config put app_hibernation app_hibernation_enabled 0
 
 adb shell settings put system min_refresh_rate 1.0
-adb shell settings put system peak_refresh_rate 48.0
-adb shell settings put secure user_refresh_rate 48.0
+adb shell settings put system peak_refresh_rate 96.0
+adb shell settings put secure user_refresh_rate 96.0
+adb shell settings put secure refresh_rate_mode 2
 
 adb shell settings put global low_power 1
 
-adb shell settings put global battery_saver_constants "advertise_is_enabled=true,datasaver_disabled=true,enable_night_mode=true,launch_boost_disabled=true,vibration_disabled=true,animation_disabled=true,soundtrigger_disabled=true,fullbackup_deferred=true,keyvaluebackup_deferred=true,firewall_disabled=true,gps_mode=0,adjust_brightness_disabled=false,adjust_brightness_factor=2,force_all_apps_standby=true,force_background_check=true,optional_sensors_disabled=true,aod_disabled=false,quick_doze_enabled=true"
+adb shell settings put global battery_saver_constants "advertise_is_enabled=true,datasaver_disabled=false,enable_night_mode=true,launch_boost_disabled=true,vibration_disabled=true,animation_disabled=true,soundtrigger_disabled=true,fullbackup_deferred=true,keyvaluebackup_deferred=true,firewall_disabled=true,gps_mode=0,adjust_brightness_disabled=false,adjust_brightness_factor=2,force_all_apps_standby=true,force_background_check=true,optional_sensors_disabled=true,aod_disabled=false,quick_doze_enabled=true"
 
 adb shell settings put global default_restrict_background_data 1
 
 
-adb shell settings put global cached_apps_freezer disabled
+adb shell settings put global cached_apps_freezer enabled
 adb shell settings put global sem_enhanced_cpu_responsiveness 0
 adb shell settings put global enhanced_processing 0
-adb shell settings put global app_standby_enabled 1
 adb shell settings put global app_restriction_enabled true
 adb shell settings put global adaptive_battery_management_enabled 0
 adb shell settings put system intelligent_sleep_mode 0
@@ -132,3 +123,17 @@ adb shell settings put system nearby_scanning_enabled 0
 adb shell settings put global ble_scan_always_enabled 0
 
 adb shell settings put system mcf_continuity 0
+
+adb shell settings put system low_priority 1
+adb shell settings put system high_priority 0
+adb shell settings put system performance_mode_on 0
+adb shell settings put system persist.sys_emc_mode power_saving
+adb shell settings put system cpu_max_speed 0
+adb shell settings put system speed_mode 0
+
+adb shell settings put global persist.hwui.profile 0
+adb shell settings put global boost_cpu 0
+adb shell settings put global sys.gpu_renderer_enhance_set 0mhz
+
+adb shell settings put global airplane_mode_on 1
+adb shell settings put global wfc_ims_mode 2
